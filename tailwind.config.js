bf9403670905a5b4560ac94aea5d2e3fd6a4251c
Benson <mark.up1109@gmail.com>
2023-06-13 14:04:05 +0800

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/*.{html,js,php}",
+    "./*.{html,js,php}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
